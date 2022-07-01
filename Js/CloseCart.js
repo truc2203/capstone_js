91257475295e8b82aea15478a6e8e4d32a992bcd
truc2203 <tranthanhtruc22031998@gmail.com>
2022-07-01 12:59:42 +0700

    fix slidebar

--- Js/CloseCart.js
+++ Js/CloseCart.js
@@ -1,5 +1,6 @@
 document.getElementById('btnCloseCart').addEventListener('click',()=>{
     document.getElementById('show-slidebar').style.right = '-100%'
     document.getElementById('show-slidebar').style.transition = 'all 0.5s'
+    document.querySelector('.cover').style.visibility = 'hidden'
     document.querySelector('.cover').style.background = 'transparent'
 })
--- Js/ShowCart.js
+++ Js/ShowCart.js
@@ -1,5 +1,6 @@
 document.getElementById('btnShowCart').addEventListener('click',()=>{
     document.getElementById('show-slidebar').style.right = '0'
     document.getElementById('show-slidebar').style.transition = 'all 0.5s'
+    document.querySelector('.cover').style.visibility = 'visible'
     document.querySelector('.cover').style.background = '#000000bf'
 })
