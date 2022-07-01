9c43148a6ec85b1a52e7d2cdfec9785e0c676d69
truc2203 <tranthanhtruc22031998@gmail.com>
2022-07-01 10:48:01 +0700

    show and close slidebar

---
+++ Js/CloseCart.js
@@ -0,0 +1,5 @@
+document.getElementById('btnCloseCart').addEventListener('click',()=>{
+    document.getElementById('show-slidebar').style.right = '-100%'
+    document.getElementById('show-slidebar').style.transition = 'all 0.5s'
+    document.querySelector('.cover').style.background = 'transparent'
+})
---
+++ Js/ShowCart.js
@@ -0,0 +1,5 @@
+document.getElementById('btnShowCart').addEventListener('click',()=>{
+    document.getElementById('show-slidebar').style.right = '0'
+    document.getElementById('show-slidebar').style.transition = 'all 0.5s'
+    document.querySelector('.cover').style.background = '#000000bf'
+})
